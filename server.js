4f41e4498c3e14244f587b68f06c8b0c14bbfe3a
thasup <thanachonfirst@hotmail.com>
2022-10-13 17:44:08 +0700

    Creating e-commerce graphql schema

--- server.js
+++ server.js
@@ -4,14 +4,63 @@ const { graphqlHTTP } = require('express-graphql');
 
 const schema = buildSchema(`
   type Query {
-    description: String
-    price: Float
+    products: [Product]
+    orders: [Order]
+  }
+
+  type Product {
+    id: ID!
+    description: String!
+    reviews: [Review]
+    price: Float!
+  }
+
+  type Review {
+    rating: Int!
+    comment: String
+  }
+
+  type Order {
+    date: String!
+    subtotal: Float!
+    items: [OrderItem]
+  }
+
+  type OrderItem {
+    product: Product!
+    quantity: Int!
   }
 `);
 
 const root = {
-  description: 'Red Shoe',
-  price: 42.45,
+  products: [
+    {
+      id: 'redshoe',
+      description: 'just a shoe',
+      price: 34.34,
+    },
+    {
+      id: 'bluehat',
+      description: 'just a hat',
+      price: 45.64,
+    },
+  ],
+  orders: [
+    {
+      date: '13-10-2022',
+      subtotal: 103.22,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'old red shoe',
+            price: 32.44,
+          },
+          quantity: 2,
+        }
+      ]
+    }
+  ]
 }
 
 const app = express();
@@ -19,6 +68,7 @@ const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
+  graphiql: true,
 }))
 
 app.listen(3000, () => {
